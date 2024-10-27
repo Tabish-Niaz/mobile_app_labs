649ee5efe55e71d5f8441e622eb2dba4b1632149
Tabish-Niaz <158143731+Tabish-Niaz@users.noreply.github.com>
2024-10-27 10:17:03 -0700

    Add files via upload

---
+++ lab4,5task1-5,prac1-15.dart
@@ -0,0 +1,289 @@
+/*import 'dart:io'; 
+void main() {
+ print("Enter your name?");
+
+ String? name = stdin.readLineSync(); 
+
+ print("Hello, $name! \nWelcome to Pakistan!!");
+}*/
+
+
+/*import 'dart:io';
+void main() {
+ print("Enter your favourite number:");
+
+ int? n =int.parse(stdin.readLineSync()!);
+
+ print("Your favourite number is $n");}*/
+
+ /*import 'dart:io';
+ void main() {
+ 
+ print("Welcome to Pakistan! from print statement");
+
+ stdout.write("Welcome to Pakistan! from stdout.write()");
+}*/
+
+
+/*import 'dart:io';
+ void main() {
+ 
+ print("Welcome to Pakistan! from print statement");
+ 
+ stdout.write("Welcome to Pakistan! from stdout.write()");
+ String s = stdin.readLineSync()!; 
+print('$s');
+ stdout.write(s);
+}*/
+/*void main() {
+ List tst = List.filled(3, null);
+ tst[0] = 'One';
+ tst[1] = 'Two';
+ tst[2] = 'Three';
+ print(tst);
+ print(tst[0]);
+}*/
+
+/*void main() {
+  
+  var myList = <int>[];
+
+  
+  myList.add(10);
+  myList.add(20);
+  myList.add(30);
+
+  
+  print('My List: $myList'); 
+
+  
+}*/
+
+/*void main() {
+  
+  var fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+
+  
+  print('First fruit: ${fruits[0]}');   
+  print('Second fruit: ${fruits[1]}');  
+  print('Third fruit: ${fruits[2]}');   
+}*/
+
+/*void main() {
+  
+  var numbers = [1, 2, 3];
+
+  
+  print('Original List: $numbers'); 
+
+  
+  numbers.add(4);
+
+  
+  print('Updated List: $numbers'); 
+}*/
+
+/*void main() {
+
+  var fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+
+  
+  print('Original List: $fruits'); 
+
+  
+  var removedElement = fruits.removeAt(2); 
+
+  
+  print('Removed Element: $removedElement'); 
+
+  
+  print('Updated List: $fruits'); 
+}*/
+
+/*void main() {
+  
+  var fruits = ['Apple', 'Banana', 'Cherry'];
+
+  
+  print('Initial length: ${fruits.length}'); 
+
+  
+  fruits.add('Date');
+  print('Length after adding an element: ${fruits.length}'); 
+
+  
+  fruits.removeAt(1); 
+  print('Length after removing an element: ${fruits.length}'); 
+  }*/
+
+  /*void main() {
+  
+  var myMap = <String, int>{};
+
+  
+  print('Original Map: $myMap'); 
+
+ 
+}*/
+
+/*void main() {
+  
+  var fruitPrices = {
+    'Apple': 1.50,
+    'Banana': 0.75,
+    'Cherry': 2.00,
+    'Date': 3.00,
+  };
+
+
+  print('Fruit Prices: $fruitPrices'); 
+  
+
+  
+}*/
+
+/*void main() {
+  
+  var studentGrades = {
+    'Alice': 'A',
+    'Bob': 'B',
+    'Charlie': 'C',
+    'Diana': 'A',
+  };
+
+  
+  print('Student Grades: $studentGrades'); 
+  
+
+  
+  String studentName = 'Bob';
+  print('Grade for $studentName: ${studentGrades[studentName]}'); 
+  
+
+ 
+}*/
+
+/*void main() {
+  // Initialize a map with key-value pairs
+  var fruitPrices = {
+    'Apple': 1.50,
+    'Banana': 0.75,
+    'Cherry': 2.00,
+  };
+
+
+  print('Original Fruit Prices: $fruitPrices'); 
+  
+
+  
+  fruitPrices['Date'] = 3.00;
+
+  
+  print('Updated Fruit Prices: $fruitPrices'); 
+  
+}*/
+
+/*void main() {
+  
+  var fruitPrices = {
+    'Apple': 1.50,
+    'Banana': 0.75,
+    'Cherry': 2.00,
+  };
+
+  
+  print('Fruit Prices: $fruitPrices'); 
+  
+
+  
+  print('Number of fruit prices: ${fruitPrices.length}'); 
+  
+
+  
+  fruitPrices['Date'] = 3.00;
+
+
+  print('Updated Fruit Prices: $fruitPrices'); 
+  
+  print('Number of fruit prices after addition: ${fruitPrices.length}'); 
+
+}*/
+
+/*void main() {
+  
+  int a = 15;
+  int b = 7;
+
+  
+  int sum = a + b;
+
+  
+  print('The sum of $a and $b is: $sum'); 
+}*/
+
+/*void main() {
+  
+  double x = 10.5;
+  double y = 3.2;
+
+
+  double product = x * y;
+
+
+  print('The product of $x and $y is: $product'); 
+}*/
+
+
+/*void main() {
+  
+  int num = -8;
+
+  
+  int absoluteValue = num.abs();
+
+  // Print the absolute value
+  print('The absolute value of $num is: $absoluteValue'); 
+}*/
+
+
+
+/*import 'dart:math';
+
+void main() {
+  
+  double decimalNum = 7.3;
+
+  
+  int ceilingValue = decimalNum.ceil();
+  int floorValue = decimalNum.floor();
+
+  
+  print('The ceiling value of $decimalNum is: $ceilingValue'); 
+  print('The floor value of $decimalNum is: $floorValue');    
+}*/
+
+/*void main() {
+  
+  int a = 15;
+  int b = 7;
+
+  if (a > b) {
+    print('$a is greater than $b'); 
+  } else {
+    print('$a is not greater than $b');
+  }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
